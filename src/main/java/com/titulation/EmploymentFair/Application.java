89446ab8454b5ba4a70f131d09a7792ba6eea2c5
Cuau cortes mejia <elzua_ecm@live.com.mx>
2019-04-04 19:54:55 -0600

    initial structure

---
+++ src/main/java/com/titulation/EmploymentFair/Application.java
@@ -0,0 +1,13 @@
+package com.titulation.EmploymentFair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
